09260e4f02c81ccafff3a57a55ba376dd998a09f
kamath-abhijith <kamath.abhijith@gmail.com>
2021-05-24 21:43:21 +0530

    wiener filter

---
+++ interp_wiener.py
@@ -0,0 +1,116 @@
+'''
+
+FAULTY SENSOR INPERPOLATION USING WIENER FILTER
+
+AUTHOR: ABIJITH J KAMATH
+abijithj@iisc.ac.in
+
+'''
+
+# %% LOAD LIBRARIES
+
+import os
+import numpy as np
+
+from matplotlib import style
+from matplotlib import rcParams
+from matplotlib import pyplot as plt
+
+import utils
+
+# %% PLOT SETTINGS
+
+plt.style.use(['science','ieee'])
+
+plt.rcParams.update({
+    "font.family": "serif",
+    "font.serif": ["cm"],
+    "mathtext.fontset": "cm",
+    "font.size": 11})
+
+# %% INITIALISATION
+
+NUM_REALISATIONS = 1000
+
+ALPHAS = np.arange(0.1,1,0.1)
+noise_var = 0.36
+idx = 40
+
+TMSE_1 = np.zeros(len(ALPHAS))
+TMSE_2 = np.zeros(len(ALPHAS))
+TMSE_3 = np.zeros(len(ALPHAS))
+
+BMSE_1 = np.zeros(len(ALPHAS))
+BMSE_2 = np.zeros(len(ALPHAS))
+BMSE_3 = np.zeros(len(ALPHAS))
+
+# %% MONTE-CARLO ANALYSIS
+
+for alpha_itr, alpha in enumerate(ALPHAS):
+    TMSE_ALPHA_1 = np.zeros(NUM_REALISATIONS)
+    TMSE_ALPHA_2 = np.zeros(NUM_REALISATIONS)
+    TMSE_ALPHA_3 = np.zeros(NUM_REALISATIONS)
+
+    BMSE_ALPHA_1 = np.zeros(NUM_REALISATIONS)
+    BMSE_ALPHA_2 = np.zeros(NUM_REALISATIONS)
+    BMSE_ALPHA_3 = np.zeros(NUM_REALISATIONS)
+
+    for itr in range(NUM_REALISATIONS):
+        signal = utils.gen_ar1(alpha, noise_var)
+        measurements = np.delete(signal, idx)
+
+        interp1, BMSE_ALPHA_1[itr] = utils.wiener_interpolator1(measurements, \
+            idx, alpha)
+        interp2, BMSE_ALPHA_2[itr] = utils.wiener_interpolator2(measurements, \
+            idx, alpha)
+        interp3, BMSE_ALPHA_3[itr] = utils.wiener_interpolator3(measurements, \
+            idx, alpha)
+
+        TMSE_ALPHA_1[itr] = (signal[idx] - interp1) ** 2
+        TMSE_ALPHA_2[itr] = (signal[idx] - interp2) ** 2
+        TMSE_ALPHA_3[itr] = (signal[idx] - interp3) ** 2
+
+    TMSE_1[alpha_itr] = np.mean(TMSE_ALPHA_1)
+    TMSE_2[alpha_itr] = np.mean(TMSE_ALPHA_2)
+    TMSE_3[alpha_itr] = np.mean(TMSE_ALPHA_3)
+    
+    BMSE_1[alpha_itr] = np.mean(BMSE_ALPHA_1)
+    BMSE_2[alpha_itr] = np.mean(BMSE_ALPHA_2)
+    BMSE_3[alpha_itr] = np.mean(BMSE_ALPHA_3)
+
+# %% PLOT SCORES
+
+os.makedirs('./results', exist_ok=True)
+path = './results/'
+
+# Wiener interpolator
+plt.figure(figsize=(12,6))
+ax = plt.gca()
+
+utils.plot_signal(ALPHAS, TMSE_1, ax=ax, plot_colour='blue', legend_label=r'TMSE',
+    xlimits=[0,1], ylimits=[0,1])
+utils.plot_signal(ALPHAS, BMSE_1, ax=ax, plot_colour='green', legend_label=r'BMSE',
+    xlimits=[0,1], ylimits=[0,1], xaxis_label=r'$\alpha$',
+    title_text=r'WIENER INTERPOLATION', save=path+'wiener1')
+
+# Two-point average interpolator
+plt.figure(figsize=(12,6))
+ax = plt.gca()
+
+utils.plot_signal(ALPHAS, TMSE_2, ax=ax, plot_colour='blue', legend_label=r'TMSE',
+    xlimits=[0,1], ylimits=[0,1])
+utils.plot_signal(ALPHAS, BMSE_2, ax=ax, plot_colour='green', legend_label=r'BMSE',
+    xlimits=[0,1], ylimits=[0,1], xaxis_label=r'$\alpha$',
+    title_text=r'TWO-POINT AVERAGE INTERPOLATION', save=path+'wiener2')
+
+# Causal Wiener interpolator
+plt.figure(figsize=(12,6))
+ax = plt.gca()
+
+utils.plot_signal(ALPHAS, TMSE_3, ax=ax, plot_colour='blue', legend_label=r'TMSE',
+    xlimits=[0,1], ylimits=[0,1])
+utils.plot_signal(ALPHAS, BMSE_3, ax=ax, plot_colour='green', legend_label=r'BMSE',
+    xlimits=[0,1], ylimits=[0,1], xaxis_label=r'$\alpha$',
+    title_text=r'CAUSAL WIENER INTERPOLATION', save=path+'wiener3')
+
+# %%
---
+++ utils.py
@@ -0,0 +1,192 @@
+'''
+
+TOOLS FOR FAULTY SENSOR INTERPOLATION
+USING WIENER AND KALMAN FILTER
+
+AUTHOR: ABIJITH J KAMATH
+abijithj@iisc.ac.in, kamath-abhijith.github.io
+
+'''
+
+# %% LOAD LIBRARIES
+
+import numpy as np
+
+from matplotlib import pyplot as plt
+
+# %% SIGNALS
+
+def gen_ar1(alpha, noise_var, num_points=100):
+    '''
+    Generate auto-regressive 1 sequence with weight alpha
+
+    :param alpha: weight
+    :param noise_var: variance of AWGN
+    :optional points: length of the sequence
+    :optional init: inital value
+
+    :return: ar1 sequence
+
+    '''
+
+    x = np.zeros(num_points)
+    x[0] = np.sqrt(noise_var/(1-alpha**2))*np.random.randn()
+    for itr in range(1,num_points):
+        x[itr] = alpha*x[itr-1] + np.sqrt(noise_var)*np.random.randn()
+
+    return x
+
+# %% OPERATORS
+
+def acorr_matrix(num_points, idx, alpha):
+    '''
+    Returns the full autocorrelation matrix for
+    full Wiener filter
+    '''
+    
+    acorr_mtx = np.zeros((num_points-1, num_points-1))
+
+    for i in range(num_points - 1):
+        for j in range(num_points - 1):
+            if np.abs(i - j) < idx:
+                acorr_mtx[i, j] = alpha ** np.abs(i - j)
+            else:
+                acorr_mtx[i, j] = alpha ** np.abs(i - j + 1)
+
+    return acorr_mtx
+
+def acorr_sequence(num_points, idx, alpha):
+    '''
+    Returns the full autocorrelation sequence for
+    full Wiener filter
+    '''
+
+    acorr_seq = np.zeros(num_points-1)
+
+    for i in range(num_points - 1):
+        if i < idx:
+            acorr_seq[i] = alpha ** (idx - i)
+        else:
+            acorr_seq[i] = alpha ** (i - idx + 1)
+
+    return acorr_seq
+
+
+# %% INTERPOLATORS
+
+def wiener_interpolator1(signal, idx, alpha, noise_var=0.36):
+    '''
+    Wiener interpolator for one point in AR1 signal
+
+    :param signal: input signal
+    :param idx: index to perform interpolation
+    :param alpha: weight of AR1 signal
+    :param noise_var: variance of awgn on signal
+
+    :return interp: interpolated point
+    :return bmse: bayesian mse
+
+    '''
+
+    num_points = len(signal) + 1
+
+    acorr_mtx = acorr_matrix(num_points, idx, alpha)
+    acorr_seq = acorr_sequence(num_points, idx, alpha)
+
+    weights = np.linalg.pinv(acorr_mtx) @ acorr_seq
+    interp = np.dot(weights, signal)
+
+    acorr_seq0 = noise_var / (1-alpha**2)
+    bmse = acorr_seq0 - (acorr_seq.T) @ np.linalg.pinv(acorr_mtx) @ acorr_seq
+
+    return interp, bmse
+
+def wiener_interpolator2(signal, idx, alpha, noise_var=0.36):
+    '''
+    Two-point average interpolator for one point in AR1 signal
+
+    :param signal: input signal
+    :param idx: index to interpolate
+    :param alpha: weight of the AR1 signal
+    :param noise_var: variance of noise on signal
+
+    :return interp: interpolation
+    :return bmse: bayesian mse
+
+    '''
+
+    interp = alpha / (1 + alpha**2) * (signal[idx-1] + signal[idx+1])
+
+    r0 = noise_var ** 2 / (1 - alpha ** 2)
+    acorr_seq = np.array([alpha*r0, alpha*r0])
+    acorr_mtx = np.array([[r0, (alpha**2)*r0], [(alpha**2)*r0, r0]])
+
+    bmse = r0 - (acorr_seq.T) @ np.linalg.pinv(acorr_mtx) @ acorr_seq
+
+    return interp, bmse
+
+def wiener_interpolator3(signal, idx, alpha, noise_var=0.36):
+    '''
+    Causal Wiener interpolator for one point in AR1 signal
+
+    :param signal: input signal
+    :param idx: index to interpolate
+    :param alpha: weight of the AR1 signal
+    :param noise_var: variance of noise on signal
+
+    :return interp: interpolation
+    :return bmse: bayesian mse
+
+    '''
+
+    num_points = len(signal) + 1
+
+    acorr_mtx = acorr_matrix(num_points, idx, alpha)
+    acorr_mtx = acorr_mtx[:idx, :idx]
+
+    acorr_seq = acorr_sequence(num_points, idx, alpha)
+    acorr_seq = acorr_seq[:idx]
+
+    weights = np.linalg.pinv(acorr_mtx) @ acorr_seq
+    interp = np.dot(weights, signal[:idx])
+
+    acorr_seq0 = noise_var / (1-alpha**2)
+    bmse = acorr_seq0 - (acorr_seq.T) @ np.linalg.pinv(acorr_mtx) @ acorr_seq
+    
+    return interp, bmse
+
+def kf():
+    pass
+
+# %% PLOTTING
+
+def plot_signal(x, y, ax=None, plot_colour='blue', xaxis_label=None,
+    yaxis_label=None, title_text=None, legend_label=None, legend_show=True,
+    legend_loc='upper left', line_style='-', line_width=None,
+    show=False, xlimits=[0,100], ylimits=[-2.5,2.5], save=None):
+    '''
+    Plots signal with abscissa in x and ordinates in y 
+
+    '''
+    if ax is None:
+        fig = plt.figure(figsize=(12,6))
+        ax = plt.gca()
+
+    plt.plot(x, y, linestyle=line_style, linewidth=line_width, color=plot_colour,
+        label=legend_label)
+    if legend_label and legend_show:
+        plt.legend(loc=legend_loc, frameon=True, framealpha=0.8, facecolor='white')
+    plt.xlabel(xaxis_label)
+    plt.ylabel(yaxis_label)
+
+    plt.xlim(xlimits)
+    plt.ylim(ylimits)
+    plt.title(title_text)
+
+    if save:
+        plt.savefig(save + '.pdf', format='pdf')
+
+    if show:
+        plt.show()
+
+    return
